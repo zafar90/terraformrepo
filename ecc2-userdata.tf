bab97ce2649a27a13d1068595a191bdc7b165c19
zafar90 <31766849+zafar90@users.noreply.github.com>
2022-04-22 22:01:56 +0530

    Create ecc2-userdata.tf

---
+++ ecc2-userdata.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  version = "~> 4.6.0"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami= "ami-04893cdb768d0f9ee"
+  instance_type = "t2.micro"
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
